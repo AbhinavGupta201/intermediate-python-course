9b6012043301dc180b37a5b12eae63fa3ec7e57f
Abhinav <guptaabhinav707@gmail.com>
2021-05-24 19:10:11 +0530

    added multiple dice rolling logic addede conditionals

--- dice_roller.py
+++ dice_roller.py
@@ -1,16 +1,22 @@
-import random 
+import random
+
 
 def main():
-  dice_rolls=2
-  dice_sum = 0
-  for i in range(0,dice_rolls):
-    roll = random.randint(1,6)
-    dice_sum = dice_sum + roll
-    # Note that the beginning and the end of the range are inclusive bounds. That is, 1 and 6 are possible values, along with integers between them.
+    dice_rolls = 2
+    dice_sum = 0
+    for i in range(0, dice_rolls):
+        roll = random.randint(1, 6)
+        dice_sum = dice_sum + roll
+        # Note that the beginning and the end of the range are inclusive bounds. That is, 1 and 6 are possible values, along with integers between them.
+        if roll == 1:
+            print(f'You rolled a {roll}! Critical Fail')
+        elif roll == 6:
+            print(f'You rolled a {roll}! Critical Success!')
+        else:
+            print(f'You rolled a {roll}')
 
-    print(f'You rolled a {roll}')
+    print(f'You have rolled a total of {dice_sum}')
 
-  print(f'You have rolled a total of {dice_sum}')
 
-if __name__== "__main__":
-  main()
+if __name__ == "__main__":
+    main()
